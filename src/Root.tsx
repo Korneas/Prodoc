d0ba1f11d955bc4948a36c4104c526ea229651df
attearturo <gomezardi@hotmail.com>
2017-11-29 01:53:42 -0500

    Error minimo de ruta y comunicacion entre componentes

--- src/Root.tsx
+++ src/Root.tsx
@@ -4,9 +4,6 @@ import { Home } from './home';
 import { MenuHome } from './menu_home';
 import { Login } from './login';
 import { CreateProject } from './create_project';
-import { Project } from './project';
-import { DocumentPrev } from './document_prev';
-import { Document } from './document';
 import * as firebase from 'firebase';
 
 export class Root extends React.Component<any, any>{
@@ -16,8 +13,7 @@ export class Root extends React.Component<any, any>{
         user: null,
         openMenu: false,
         openNotify: false,
-        addProject: false,
-        expoProject: -1
+        addProject: false
     }
 
     componentWillMount() {
@@ -68,12 +64,6 @@ export class Root extends React.Component<any, any>{
         });
     }
 
-    onViewProject = (actualProject) => {
-        this.setState({
-            expoProject: actualProject
-        });
-    }
-
     render() {
         //Si el usuario NO esta logueado
         if (!this.state.user) {
@@ -82,21 +72,18 @@ export class Root extends React.Component<any, any>{
             )
         } else {
             //Si esta logueado
-            if (this.state.expoProject == -1) {
-                return (
-                    <div className='main'>
-                        <BottomBar onOpenWindow={(getOpenMenu, getOpenNotify) => { this.setState({ openMenu: getOpenMenu, openNotify: getOpenNotify }); }} onCreateProject={this.onCreateProject} />
-                        <MenuHome move={this.state.openMenu} currentUser={this.state.user} currentProjects={this.state.projects} onLogOut={(cmd) => this.onLogOut(cmd)} />
-                        <Home moveRight={this.state.openMenu} moveLeft={this.state.openNotify} currentUser={this.state.user} currentProjects={this.state.projects} onProject={(num) => {this.onViewProject(num); console.log("Estoy en el proyecto: " + num)}}/>
+            return (
+                <div className='main'>
+                    <BottomBar onOpenWindow={(getOpenMenu, getOpenNotify) => { this.setState({ openMenu: getOpenMenu, openNotify: getOpenNotify }); }} onCreateProject={this.onCreateProject} />
+                    <MenuHome move={this.state.openMenu} currentUser={this.state.user} currentProjects={this.state.projects} onLogOut={(cmd) => this.onLogOut(cmd)} />
+                    <Home moveRight={this.state.openMenu} moveLeft={this.state.openNotify} currentUser={this.state.user} currentProjects={this.state.projects} />
 
-                        {this.state.addProject ? (
-                            <CreateProject creatingProject={this.state.addProject} />
-                        ) : null}
-                    </div>
-                )
-            } else {
-                return <Project  project={this.state.projects[this.state.expoProject]}/>;
-            }
+                    {this.state.addProject ? (
+                        <CreateProject creatingProject={this.state.addProject} />
+                    ) : null}
+
+                </div>
+            )
         }
     }
 }
--- src/login.tsx
+++ src/login.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import * as firebase from 'firebase';
 import Dropzone from 'react-dropzone';
 
 export class Login extends React.Component<any, any>{
@@ -6,12 +7,53 @@ export class Login extends React.Component<any, any>{
     state = {
         user: null,
         imagen: null,
-        pagina: 'login',
+        pagina: 'login'
     }
+
     getMetodos() {
+        this.handleAuth = this.handleAuth.bind(this);
+        this.handleLogout = this.handleLogout.bind(this);
     }
     componentWillMount() {
         this.getMetodos();
+
+        //Sincronizacion con autenticacion
+        firebase.auth().onAuthStateChanged(user => {
+            console.log('Ingreso al usuario');
+
+            var params = new URLSearchParams();
+            params.append('email', user.displayName);
+            params.append('psw', user.email);
+
+            fetch('http://localhost:5000/login', {
+                method: 'post',
+                body: params
+            })
+                .then(e => e.json())
+                .then(res => {
+                    this.setState({
+                        mensaje: res.mensaje
+                    });
+                    console.log(res);
+                    if (res.response == 'valid') {
+                        localStorage.setItem('user', JSON.stringify(res.user));
+
+                        this.props.setData(res.user, res.projects);
+                        this.props.cambioPagina('home');
+                    }
+                });
+        });
+    }
+    handleAuth() {
+        const proveedorGoogle = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().signInWithPopup(proveedorGoogle)
+            .then(result => console.log(`${result.user.email} Ha iniciado sesión`), )
+            .catch(error => console.log(`Error ${error.code}: ${error.message}`));
+    }
+    handleLogout() {
+        firebase.auth().signOut()
+            .then(result => console.log(`${result.user.email} Ha cerrado sesión`), )
+            .catch(error => console.log(`Error ${error.code}: ${error.message}`));
     }
     hacerLogin(evento) {
         evento.preventDefault();
@@ -32,7 +74,7 @@ export class Login extends React.Component<any, any>{
                 console.log(res);
                 if (res.response == 'valid') {
                     localStorage.setItem('user', JSON.stringify(res.user));
-                    this.props.setUsuario(res.user);
+                    this.props.setData(res.user, res.projects);
                 }
             });
     }
@@ -62,6 +104,7 @@ export class Login extends React.Component<any, any>{
                 }
             });
     };
+
     cambioPagina(nuevaPagina) {
         this.setState({
             pagina: nuevaPagina
@@ -80,23 +123,25 @@ export class Login extends React.Component<any, any>{
                     <div id="bodyLogin">
                         <form className='login' onSubmit={e => this.hacerLogin(e)}>
                             <img className="logo" src="public/img/logo.png" />
-                            <input id='usern' type="email" name="user" placeholder="CORREO ELECTRONICO" required />
-                            <input id='psw' type="password" name="psw" placeholder="CONTRASEÑA" required />
-                            <button id='entrada' type="submit" className="btnLogin btnLogin-primary btnLogin-block btnLogin-large">INGRESAR</button>
-                            <p className="subTexto">¿No tienes una cuenta?</p>
-
-                            <p><a className="registro" href='#' onClick={() => this.setState({ pagina: 'registro' })}>CREAR CUENTA</a></p>
+                            <h4 className='subHeader'>Just design better.</h4>
+                            <input id='usern' type="email" name="user" placeholder="email" required />
+                            <input id='psw' type="password" name="psw" placeholder="contraseña" required />
+                            <button id='entrada' type="submit" className="btnLogin btnLogin-primary btnLogin-block btnLogin-large">Entrar</button>
+                            <p className="subTexto">¿Eres nuevo? <a className="registro" href='#' onClick={() => this.setState({ pagina: 'registro' })}  >Crear una cuenta</a></p>
+                            <button id='entrada'
+                                type="submit"
+                                className="btnLogin btnLogin-primary btnLogin-block btnLogin-large google"
+                                onClick={this.handleAuth}>Empezar con Google</button>
                         </form>
                     </div>
                 </div>
             case 'registro':
-                return <div id="login">
-                    <div id="bodyLogin" className='landing'>
+                return <div id="register" className='landing'>
+                    <div id="bodyRegister">
                         <form className='login' onSubmit={e => this.hacerRegistro(e)}>
-                            <img className="logo" src="public/img/logo.png" />
-                            <input id='name' type="text" name="newUser" placeholder="NOMBRE" required />
-                            <input id='usern' type="email" name="email" placeholder="CORREO ELECTRONICO" required />
-                            <input id='psw' type="password" name="psw" placeholder="CONTRASEÑA" required />
+                            <h4 className='subHeader'>Bienvenido a Prodoc</h4>
+                            <p className='subText'>Documenta tus proyectos fácilmente para que hagas más lo que importa: Diseñar.</p>
+
                             {!this.state.imagen && <Dropzone
                                 className='cargarFotoUsuario' activeClassName='overCargarFoto'
                                 onDrop={(accepted, rejected) => this.cargarImagen(accepted, rejected)}
@@ -107,10 +152,14 @@ export class Login extends React.Component<any, any>{
                                     <img className='preImagen' style={{ height: 150, textAlign: 'center', display: 'block' }} src={this.state.imagen.preview} />
                                 </div>
                             }
-                            <button id='entrada' type="submit" className="btnLogin btnLogin-primary btnLogin-block btnLogin-large">REGISTRARSE</button>
-                            <p className="subTexto">¿Ya tienes una cuenta?</p>
-                            <p><a className="registro" href='#' onClick={() => this.setState({ pagina: 'login' })}  >INICIAR SESIÓN</a></p>
+                            <input id='name' type="text" name="newUser" placeholder="Nombre" required />
+                            <input id='usern' type="email" name="email" placeholder="Email" required />
+                            <input id='psw' type="password" name="psw" placeholder="Contraseña" required />
+                            <button id='entrada' type="submit" className="btnRegister btnLogin-primary btnLogin-block btnLogin-large">Empezar</button>
+                            <p className="subText">¿Ya eres usuario? <a className="registro" href='#' onClick={() => this.setState({ pagina: 'login' })}  >Accede a tu cuenta</a></p>
                         </form>
+
+
                     </div>
                 </div>
         }
